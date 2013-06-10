813558822fd87fde56c5a01768386542ed19c4cc
rpq <rpq@winscores.com>
2013-06-10 19:16:27 -0400

    username lowercase on login

--- authentication/forms.py
+++ authentication/forms.py
@@ -3,3 +3,6 @@ from django import forms
 class LoginForm(forms.Form):
     username = forms.CharField(required=True)
     password = forms.CharField(required=True, widget=forms.PasswordInput)
+
+    def clean_username(self):
+        return self.cleaned_data['username'].lower()
