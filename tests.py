11f4a7593edf427907423181ba57071153114ade
rpq <rpq@winscores.com>
2013-06-27 10:42:37 -0400

    begin adding tests

--- tests.py
+++ tests.py
@@ -1,16 +1,24 @@
-"""
-This file demonstrates writing tests using the unittest module. These will pass
-when you run "manage.py test".
+from django.utils import unittest
+from django.test.client import Client
 
-Replace this with more appropriate tests for your application.
-"""
+TESTSERVER_HOST = 'testserver'
+TESTSERVER_ROOT_URL = 'http://{0}'.format(TESTSERVER_HOST)
 
-from django.test import TestCase
+class LoginTest(unittest.TestCase):
 
+    def setUp(self):
+        self.client = Client(enforce_csrf_checks=True)
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
-        """
-        Tests that 1 + 1 always equals 2.
-        """
-        self.assertEqual(1 + 1, 2)
+    def test_login_page_redirect(self):
+        REDIRECT_URLS = ['/login-redirect', '/login',]
+        r = self.client.get('/', follow=True)
+
+        self.assertIn(TESTSERVER_ROOT_URL + REDIRECT_URLS[0],
+            dict(r.redirect_chain).keys())
+        self.assertIn(TESTSERVER_ROOT_URL + REDIRECT_URLS[1],
+            dict(r.redirect_chain).keys())
+        self.assertEqual(r.status_code, 200)
+
+    def test_login_page_status_code(self):
+        r = self.client.get('/login')
+        self.assertEqual(r.status_code, 200)
