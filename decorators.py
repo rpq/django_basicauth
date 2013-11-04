25b05c3740a693efe814ec0f6acd4112f42ac47e
ramon quezada <rpq@winscores.com>
2013-11-04 13:53:32 -0500

    remove admin_access decorator, move somewhere else, later

--- decorators.py
+++ decorators.py
@@ -18,35 +18,6 @@ def login_required(func):
                 return redirect('login')
     return wrapper
 
-'''
-# for group related stuff
-
-def admin_access(func):
-    def wrapper(request, *args, **kwargs):
-        if _logged_in(request):
-            logged_in_user_id = \
-                request.session.get('logged_in_user_id', None)
-            if logged_in_user_id is None:
-                return _display_access_message_and_redirect(request)
-
-            try:
-                user = models.User.objects.get(pk=logged_in_user_id)
-                if user.is_admin():
-                    return func(request, *args, **kwargs)
-                else:
-                    return _display_access_message_and_redirect(
-                        request)
-            except models.User.DoesNotExist:
-                return _display_access_message_and_redirect(request)
-        else:
-            if request.is_ajax():
-                return helpers.json_redirect(reverse('login'))
-            else:
-                messages.error(request, 'Please login to view this page.')
-                return redirect('login')
-    return wrapper
-'''
-
 def _logged_in(request):
     if request.session.get('logged_in_user_id', None) is not None:
         return True
