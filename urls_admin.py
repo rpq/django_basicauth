c3bc207001eafce5a9cc2a67e06b748eee6a0663
ramon quezada <rpq@winscores.com>
2013-11-26 14:43:59 -0500

    add urls.py login routes

--- urls_admin.py
+++ urls_admin.py
@@ -4,28 +4,25 @@ from django.views.generic.base import RedirectView
 urlpatterns = patterns(
     'basicauth.views_admin',
 
-    url(r'^$',
-        RedirectView.as_view(url='/admin/basicauth/user/list'),
-        name='basicauth_root'),
-    url(r'/user/new$', 'user_new', name='user_new'),
-    url(r'/user/create$', 'user_create', name='user_create'),
-    url(r'/user/list$', 'user_list', name='user_list'),
-    url(r'/user/(?P<user_id>\d+)/detail$',
-        'user_detail', name='user_detail'),
-    url(r'/user/(?P<user_id>\d+)/edit$',
-        'user_edit', name='user_edit'),
-    url(r'/user/(?P<user_id>\d+)/update$',
-        'user_update', name='user_update'),
-    url(r'/user/(?P<user_id>\d+)/delete$',
-        'user_delete', name='user_delete'),
+'''
+These should go into the your project's urls.py:
 
-    url(r'/user/(?P<user_id>\d+)/usergroup/new$',
-        'ajax_usergroup_new', name='ajax_usergroup_new'),
-    url(r'/usergroup/create$',
-        'ajax_usergroup_create', name='ajax_usergroup_create'),
-    url(r'/usergroup/' \
-        '(?P<usergroup_id>\d+)/delete$',
-        'usergroup_delete', name='usergroup_delete'),
+   url(r'^/?$',
+        RedirectView.as_view(url=settings.LOGIN_SUCCESS_DEFAULT_URL),
+        name='access_root'),
+   url(r'^success$',
+        RedirectView.as_view(url=settings.LOGIN_SUCCESS_DEFAULT_URL),
+        name='root'),
+   url(r'^login-redirect$',
+        'basicauth.authentication.views.login_redirect',
+        name='login_redirect'),
+   url(r'^login$',
+        'basicauth.authentication.views.login',
+        name='login'),
+   url(r'^logout$',
+        'basicauth.authentication.views.logout',
+        name='logout'),
+'''
 
 '''
     url(r'^register$', 'basicauth.authentication.views.register',
